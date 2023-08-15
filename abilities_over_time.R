0ddf95bb982aa837f94b959a1b797a17a8bc453c
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2023-08-15 10:14:25 +0200

    Uploaded abilities_over_time.R and plot_parameters_ts.R

---
+++ abilities_over_time.R
@@ -0,0 +1,41 @@
+#_______________________________________________________________________________         
+#   About: football abilities over the second half of the league
+#_______________________________________________________________________________     
+
+#-------------------------------------------------------------------------------
+# Import data, libraries and functions
+serieA_2122<- read.csv("../data/serieA_21-22.csv")
+teams <- names(table(serieA_2122[,"HomeTeam"]))
+library(ggplot2)
+source("functions/plot_parameters_ts.R")
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# Examples of running the function
+plot_parameters_ts('Milan')
+plot_parameters_ts('Roma')
+plot_parameters_ts('Napoli')
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# Apply fuction plot_parameters_ts() to all teams of the league
+Plots = lapply(1:20, function(i){
+    #Get current team name
+    team= teams[i]
+    # Time series of team abilities
+    plot_parameters_ts(team)
+})
+
+# Unique grid with all the plots
+# (result will change according to dimensions of the plot panel in Rstudio)
+
+#install.packages("patchwork")
+library(patchwork)
+(Plots[[1]] | Plots[[2]] |Plots[[3]] | Plots[[4]]) /
+    (Plots[[5]] | Plots[[6]] |Plots[[7]] | Plots[[8]]) /
+    (Plots[[9]] | Plots[[10]] |Plots[[11]] | Plots[[12]]) /
+    (Plots[[13]] | Plots[[14]] |Plots[[15]] | Plots[[16]]) /
+    (Plots[[17]] | Plots[[18]] |Plots[[19]] | Plots[[20]])
+#-------------------------------------------------------------------------------
---
+++ functions/plot_parameters_ts.R
@@ -0,0 +1,54 @@
+#_______________________________________________________________________________         
+#   About: ~ This function receive a team name as input and return a plot
+#            that shows team abilities over the second half of the league
+#          ~ Attack strength represented in blue
+#          ~ Defence weakness represented in red
+#_______________________________________________________________________________  
+
+plot_parameters_ts<- function(team){
+    #---------------------------------------------------------------------------
+    # Get team abilities
+    team_att<-vector(mode="numeric",length=19)
+    team_def<-vector(mode="numeric",length=19)
+    #---------------------------------------------------------------------------
+    for (match in 20:38){
+        #.......................................................................
+        # Load parameters estimated for the current matchday
+        filepath<- paste("parameters/xi/par_list_",match,".RData",sep="")
+        load(filepath)
+        #.......................................................................
+        # Select parameters associated to the best xi (from profile loglike)
+        # Optimal parameters are in range 21:24 (-->xi=0.005) of par_list
+        best_xi_par<-par_list[21:24] 
+        #.......................................................................
+        # Insert parameters into abiliy arrays
+        i<-match-19
+        team_att[i]<- as.numeric(best_xi_par$att[team])
+        team_def[i]<- as.numeric(best_xi_par$def[team])
+        #.......................................................................
+    }
+    #---------------------------------------------------------------------------
+    # Create a dataframe of team abilities
+    team_df<- data.frame(cbind(matchday=19:37,team_att,team_def))
+    #---------------------------------------------------------------------------
+    # Abilities over time plot
+    plot_abilities<- ggplot(data = team_df, aes(x = matchday)) +
+        geom_line(aes(y = team_att,colour="#154360"))+
+        geom_point(aes(y = team_att, colour = "#154360"),
+                   size = 2)+
+        geom_line(aes(y = team_def,colour="#BB1B0B"))+
+        geom_point(aes(y = team_def,colour = "#BB1B0B"),
+                   size = 2)+
+        xlab("Matchday")+
+        ylab("Coefficients")+
+        ggtitle(paste(team,"abilities over time",sep=" "))+
+        scale_color_manual(name="Cofficient Type",
+                           labels=c("Attack Strength", "Defence Weakness"),
+                           values=c("#154360","#BB1B0B"))+
+        theme(plot.title = element_text(hjust = 0.5,face="bold"),
+              axis.text=element_text(size=10),
+              axis.title=element_text(size=10,face="bold"))
+    #---------------------------------------------------------------------------
+    # Return the plot
+    return(plot_abilities)
+}
